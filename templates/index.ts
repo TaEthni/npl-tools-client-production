69d7cb3b17c45b680adb0607dda3a51b71eae8a6
GH_USERNAME <GH_EMAIL>
2019-12-29 16:28:53 -0600

    Auto-generated commit

--- templates/index.ts
+++ templates/index.ts
@@ -3,6 +3,8 @@ import { ChurchCircles12Template } from './church-circles-12/template';
 import { ChurchCirclesConfiguration } from './church-circles/configuration-new';
 import { ChurchCirclesDefaultTemplate } from './church-circles/template';
 import { translations } from './translations';
+import { ChurchCirclesCzechConfiguration } from './church-circles-czech/configuration-new';
+import { ChurchCirclesCzechTemplate } from './church-circles-czech/template';
 export * from './countries';
 export * from './template.interface';
 
@@ -11,14 +13,22 @@ export { GenMapperConfigs, GenMapperTemplates, translations };
 const GenMapperTemplates = [
     ChurchCirclesDefaultTemplate,
     ChurchCircles12Template,
+    ChurchCirclesCzechTemplate
     // ChurchCirclesEastTemplate,
 ];
 
 const GenMapperConfigs = [
     ChurchCirclesConfiguration,
     ChurchCircles12Configuration,
+    ChurchCirclesCzechConfiguration
     // ChurchCirclesEastConfiguration
 ];
+
+export const GMTemplates = {
+    ChurchCirclesDefaultTemplate,
+    ChurchCircles12Template,
+    ChurchCirclesCzechTemplate
+}
 
 
 // Remove
