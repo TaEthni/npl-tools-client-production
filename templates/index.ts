abfd22c98088c7ac457b741a1bf8059823fe9f4c
GH_USERNAME <GH_EMAIL>
2019-12-29 14:36:02 -0600

    Auto-generated commit

---
+++ templates/index.ts
@@ -0,0 +1,25 @@
+import { ChurchCircles12Configuration } from './church-circles-12/configuration-new';
+import { ChurchCircles12Template } from './church-circles-12/template';
+import { ChurchCirclesConfiguration } from './church-circles/configuration-new';
+import { ChurchCirclesDefaultTemplate } from './church-circles/template';
+import { translations } from './translations';
+export * from './countries';
+export * from './template.interface';
+
+export { GenMapperConfigs, GenMapperTemplates, translations };
+
+const GenMapperTemplates = [
+    ChurchCirclesDefaultTemplate,
+    ChurchCircles12Template,
+    // ChurchCirclesEastTemplate,
+];
+
+const GenMapperConfigs = [
+    ChurchCirclesConfiguration,
+    ChurchCircles12Configuration,
+    // ChurchCirclesEastConfiguration
+];
+
+
+// Remove
+export interface GMStreamAttribute { }
